f273a5a16c3b80bfed728e976dbd04376b7f1ec1
Expurgator <timkayhou@gmail.com>
2022-03-20 02:01:18 +0900

    🐳 chore: vue-router

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,9 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 //@ts-ignore
+import router from './router'
+//@ts-ignore
 import store from './store'
 
-createApp(App).use(store).mount('#app')
+
+createApp(App).use(router).use(store).mount('#app')
