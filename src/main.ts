63683a9348057f01e5e675250f2ce75ea72b2b3a
Expurgator <timkayhou@gmail.com>
2022-03-20 02:10:58 +0900

    🐞 fix: vuex vue-router ts

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,6 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-//@ts-ignore
 import router from './router'
-//@ts-ignore
 import store from './store'
-
 
 createApp(App).use(router).use(store).mount('#app')
