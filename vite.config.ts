150762cce686020859fd314e0573491539e361e3
Expurgator <timkayhou@gmail.com>
2022-03-19 23:03:22 +0900

    ✨ feat: SFC

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,26 @@
 import vue from '@vitejs/plugin-vue'
 import { defineConfig } from 'vite'
+import { viteSingleFile } from "vite-plugin-singlefile"
 
 // https://vitejs.dev/config/
 export default defineConfig( {
   server: {
     host: '0.0.0.0'
   },
-  plugins: [ vue() ],
+  plugins: [
+    vue(),
+    viteSingleFile()
+  ],
   build: {
+    target: "esnext",
+    outDir: 'dist',
+    assetsInlineLimit: 100000000,
+    chunkSizeWarningLimit: 100000000,
+    cssCodeSplit: false,
+    brotliSize: false,
     rollupOptions: {
       output: {
+        manualChunks: () => "everything.js",
         entryFileNames: 'assets/[name].js',
         chunkFileNames: 'assets/[name].js',
         assetFileNames: 'assets/[name].[ext]'
