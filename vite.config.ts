61e61f2ed5d73fcda8393ad51ccc4eb43074ac12
Expurgator <timkayhou@gmail.com>
2022-03-19 22:58:03 +0900

    🎈 perf: dist no hash

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,19 @@
-import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [vue()]
+export default defineConfig( {
+  server: {
+    host: '0.0.0.0'
+  },
+  plugins: [ vue() ],
+  build: {
+    rollupOptions: {
+      output: {
+        entryFileNames: 'assets/[name].js',
+        chunkFileNames: 'assets/[name].js',
+        assetFileNames: 'assets/[name].[ext]'
+      }
+    }
+  }
 })
